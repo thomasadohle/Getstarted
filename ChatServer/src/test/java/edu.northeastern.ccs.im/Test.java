d3ccbf98340af534281a74b6b9229235201e442c
thomasadohle <dohle.t@husky.neu.edu>
2019-02-18 09:34:23 -0600

    tested MessageType and Message

--- ChatServer/src/test/java/edu.northeastern.ccs.im/Test.java
+++ ChatServer/src/test/java/edu.northeastern.ccs.im/Test.java
@@ -1,15 +1,65 @@
 package edu.northeastern.ccs.im;
 
-public class Test {
 
+class Test {
+
+  /**
+   * Test MessageType enum.
+   */
   @org.junit.jupiter.api.Test
-  public void testMessage(){
+  void testMessageType() {
     MessageType hello = MessageType.HELLO;
     MessageType bye = MessageType.QUIT;
     MessageType bcst = MessageType.BROADCAST;
-    assert("HLO" == hello.toString());
-    assert("BYE" == bye.toString());
-    assert("BCT" == bcst.toString());
+    assert ("HLO".equals(hello.toString()));
+    assert ("BYE".equals(bye.toString()));
+    assert ("BCT".equals(bcst.toString()));
+  }
+
+  /**
+   * Test Message.java methods except makeMessage and toString
+   */
+  @org.junit.jupiter.api.Test
+  void testMessage1() {
+    String name = "Thomas";
+    Message quit = Message.makeQuitMessage(name);
+    assert (quit.getName().equals(name));
+    Message bcst = Message.makeBroadcastMessage(name, "Nice weather");
+    assert (bcst.getText().equals("Nice weather"));
+    Message hello = Message.makeHelloMessage("Hi there!");
+    assert (hello.getName() == null);
+    Message simpleHello = Message.makeSimpleLoginMessage(name);
+    assert (simpleHello.getText() == null);
+    assert (quit.terminate());
+    assert (hello.isInitialization());
+    assert (bcst.isBroadcastMessage());
+  }
+
+  /**
+   * Test Message.java method makeMessage
+   */
+  @org.junit.jupiter.api.Test
+  void testMessage2() {
+    String name = "Charles Darwin";
+    Message quit = Message.makeMessage("BYE", name, null);
+    assert (quit.terminate());
+    Message hello = Message.makeMessage("HLO", name, null);
+    assert (hello.isInitialization());
+    Message broadcast = Message.makeMessage("BCT", name, "I have tried lately to read " +
+            "Shakespeare, and found it so intolerably dull that it nauseated me.");
+    assert (broadcast.isBroadcastMessage());
+  }
+
+  /**
+   * Test Message.java method toString
+   */
+  @org.junit.jupiter.api.Test
+  void testMessage3() {
+    String user = "Aristotle";
+   Message bcst1 = Message.makeBroadcastMessage(null,"update: it is cold");
+   assert(bcst1.toString().equals("BCT 2 -- 18 update: it is cold"));
+   Message bcst2 = Message.makeBroadcastMessage(user,null);
+   assert(bcst2.toString().equals("BCT 9 Aristotle 2 --"));
   }
 
 
